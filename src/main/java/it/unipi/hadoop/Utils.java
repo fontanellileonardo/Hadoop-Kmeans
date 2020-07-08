36ce306dde58805ef9e654a1fd91c263ed6355e0
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2020-07-08 10:23:42 +0200

    Changed number of iterations

--- src/main/java/it/unipi/hadoop/Utils.java
+++ src/main/java/it/unipi/hadoop/Utils.java
@@ -2,7 +2,7 @@ package it.unipi.hadoop;
 
 public class Utils {
     // Maximum number of iterations after which the algorithm terminates
-    protected final static int MAX_ITERATIONS = 2;
+    protected final static int MAX_ITERATIONS = 10;
     // Minimum centroids shitf that, if achieved, the algorithm terminates
     protected final static double MIN_SHIFT = 0.05;
     // String of output file common to all the reducers
