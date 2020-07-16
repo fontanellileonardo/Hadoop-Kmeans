ba3d4870f0348cdfd1da2d3d1613d8e1fad680ff
Leonardo Fontanelli <l.fontanelli2@studenti.unipi.it>
2020-07-16 21:21:37 +0200

    Changed the istantiation of the Point variable

--- src/main/java/it/unipi/hadoop/KmeansCombiner.java
+++ src/main/java/it/unipi/hadoop/KmeansCombiner.java
@@ -8,16 +8,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class KmeansCombiner extends Reducer<Centroid, Point, Centroid, Point> {
 
     private final static Point pointSum = new Point();
-    private final static Point p = new Point();
 
     public void reduce(Centroid id, Iterable<Point> points, Context context) throws IOException, InterruptedException{
         final Iterator<Point> it = points.iterator();
         pointSum.set(it.next());
         // Parse every point 
         while(it.hasNext()){
-            p.set(it.next());
             // Add every coordinate of the points
-            pointSum.add(p);
+            pointSum.add(it.next());
         }
         context.write(id, pointSum);
     }
